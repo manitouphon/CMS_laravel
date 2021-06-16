4421869176671bdac18bb776193184b07ab611fe
Matthew Price <manitophon.official@gmail.com>
2021-06-16 12:00:19 +0700

    Finishing CalculatingAvailability from Staff::Model
    You can now call Calculate availability to refresh status_day && status_hour.

--- app/Models/Staff.php
+++ app/Models/Staff.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Http\Resources\WorkingScheduleResource;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\WorkingSchedule;
@@ -30,41 +31,79 @@ class Staff extends Model
 
     public static function checkIfDoctorAvialable($doctor_id)
     {
-        $this->calculateAvailability($doctor_id);
+        Staff::calculateAvailability($doctor_id);
         $staffWorkhours = new WorkingScheduleResource(WorkingSchedule::findOrFail($doctor_id));
         return ($staffWorkhours->status_day === 1 && $staffWorkhours->status_hour === 1);
     }
 
     public static function calculateAvailability($doctor_id){
-        //TODO: discussion[dooctor could only have 1 working schedule ];
-       $schedules = WorkingSchedule::where('staff_id',$doctor_id)->first();
-    //    $schedules = $schedule 
-       $workingDays = $schedules->working_day;
-       
+        //DoneTODO: discussion[doctor could only have 1 working schedule ]; @Manitou
 
-       $schDay = [false,false,false,false,false,false,false];
+       $schedules = WorkingSchedule::where('staff_id',$doctor_id)->first();
+       $workingDays = $schedules->working_day;
+
+
+       $bool_workingDays = [false,false,false,false,false,false,false];
        for($i = 0; $i < strlen($workingDays); $i++){
             if($workingDays[$i] === '1'){
-                $schDay[$i] = true;
+                $bool_workingDays[$i] = true;
             }
             else{
-                $schDay[$i] = false;
+                $bool_workingDays[$i] = false;
             }
        }
 
-       return[
-            'sun' => $schDay[0],
-            'mon' => $schDay[1],
-            'tue' => $schDay[2],
-            'wed' => $schDay[3],
-            'thu' => $schDay[4],
-            'fri' => $schDay[5],
-            'sat' => $schDay[6]
-        ];
-       //STATUS: Working Fine.
+        //TODO: Calculate the time and set status_day and status_hour@Manitou
+        //Calculating availability and set it to status_day && status_hour;
+        $now = Carbon::now();
 
-       //TODO: Calculate the time and set status_day and status_hour@Manitou
-       
+       //Calculate: status_day
+        $expected_day = [
+            'Sun' => $bool_workingDays[0],
+            'Mon' => $bool_workingDays[1],
+            'Tue' => $bool_workingDays[2],
+            'Wed' => $bool_workingDays[3],
+            'Thu' => $bool_workingDays[4],
+            'Fri' => $bool_workingDays[5],
+            'Sat' => $bool_workingDays[6]
+        ];
+        $day = $now->isoFormat('ddd');
+        foreach ($expected_day as $ddd => $value){
+            if($day === $ddd){
+                if($value === true){
+                    WorkingSchedule::find($schedules->id)->update(['status_day'=>'1']);
+                }//If $value === 1
+                else{
+                    WorkingSchedule::find($schedules->id)->update(['status_day'=>'0']);
+                }
+            }//If match $ddd from $expected array
+        }
+
+        //Calculate: status_hour
+        $now->hour = $now->hour+7; //Default it will be UTC which has GTM == + 0; Cambodia has GTM +7
+        $start_time = Carbon::parse($schedules->start_time);
+        $end_time = Carbon::parse($schedules->end_time);
+
+            //Check if $now is in the range of $start_time && $end_time
+        if($now->lessThanOrEqualTo($end_time) && $now->greaterThanOrEqualTo($start_time) ){
+            WorkingSchedule::find($schedules->id)->update(['status_hour'=>'1']);
+
+        }
+
+
+
+
+        //PURPOSE::TESTING
+        return[
+            'sun' => $bool_workingDays[0],
+            'mon' => $bool_workingDays[1],
+            'tue' => $bool_workingDays[2],
+            'wed' => $bool_workingDays[3],
+            'thu' => $bool_workingDays[4],
+            'fri' => $bool_workingDays[5],
+            'sat' => $bool_workingDays[6]
+        ];
+        //STATUS: Working Fine.
 
 
     }
